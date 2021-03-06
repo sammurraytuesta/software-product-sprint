a506cf347e8af8d4ffbcd57014538f5d3aae6326
Samantha <smurraytuesta@gmail.com>
2021-03-06 06:44:49 +0000

    Add servlet to say Hello World after button is clicked

---
+++ portfolio/src/main/webapp/js/servlet.js
@@ -0,0 +1,9 @@
+/** Fetches the current date from the server and adds it to the page. */
+    async function sayHello() {
+    const responseFromServer = await fetch('/hello');
+    const textFromResponse = await responseFromServer.text();
+
+    const helloContainer = document.getElementById('hello-container');
+    helloContainer.innerText = textFromResponse;
+    }
+    
