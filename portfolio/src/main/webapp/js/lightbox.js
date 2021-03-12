58617d2823fbcc0c2e1f6ea758fe5df3074673ed
Samantha <smurraytuesta@gmail.com>
2021-03-12 04:46:37 +0000

    Clean up design-edits branch to only have design edits

--- portfolio/src/main/webapp/js/lightbox.js
+++
@@ -1,4 +0,0 @@
-
-$('document').ready(function () {
-    baguetteBox.run('.baguetteBoxOne');
-});
--- portfolio/src/main/webapp/js/servlet.js
+++
@@ -1,9 +0,0 @@
-/** Fetches the current date from the server and adds it to the page. */
-    async function sayHello() {
-    const responseFromServer = await fetch('/hello');
-    const textFromResponse = await responseFromServer.text();
-
-    const helloContainer = document.getElementById('hello-container');
-    helloContainer.innerText = textFromResponse;
-    }
-    
