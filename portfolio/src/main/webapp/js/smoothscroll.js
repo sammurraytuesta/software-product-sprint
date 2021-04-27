882b6be56663255c0b638354c85de0ebbab3d044
samantha murray <60449164+sammurraytuesta@users.noreply.github.com>
2021-04-26 23:18:39 -0700

    remove commented out code

--- portfolio/src/main/webapp/js/smoothscroll.js
+++ portfolio/src/main/webapp/js/smoothscroll.js
@@ -1,25 +1,11 @@
 $('document').ready(()=>{
-//  console.log('scroll');
+    
     $('.scroll').click(function(e){
-        //console.log('scroll');
         e.preventDefault();
         $('html,body').animate({
             scrollTop: $(this.hash).offset().top
         }, 2000)
     });
-
-    /* scrollreveal -  */
-
-    //scrollReveal().reveal('.showcase-text');
-
-   /*  sr.reveal('.showcase-text',{
-        origin: 'down',
-        distance: '100px',
-        duration: 2000,
-        scale: '0.5',
-        delay: 2000
-
-    }); */
 
     const sr = ScrollReveal({
         origin: 'down',
